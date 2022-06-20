24e4e16f616c0d9e5ef52d1914e2e21d282650f2
Yakup KAYA <iamyakupkaya@outlook.com>
2022-06-20 15:08:26 +0300

    I made put and deletion requests with postman on sever

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -3,6 +3,7 @@ const app = express();
 const Joi = require("joi");
 app.use(express.json()); // it is necessary to post. that means express have a body also in body have a json so split it.
 
+/* XXXXXXXXXXXXXXXXXXXXXX | LOCAL API | XXXXXXXXXXXXXXXXXXXXXX */
 const users = [
   {
     id: 1,
@@ -63,15 +64,29 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-/* XXXXXXXXXXXXXXXXXXXXXX | POST | XXXXXXXXXXXXXXXXXXXXXXXX*/
-
-app.post("/users", (request, response) => {
-  //Using Joi to validate data
+const getSchema = () => {
   const schema = Joi.object({
     name: Joi.string().min(2).max(30).required(),
     surname: Joi.string().min(2).max(30).required(),
     age: Joi.number().integer().max(199).required(),
   });
+
+  return schema;
+};
+
+const getRequestBody = (request) => {
+  return {
+    name: request.body.name,
+    surname: request.body.surname,
+    age: request.body.age,
+  };
+};
+
+/* XXXXXXXXXXXXXXXXXXXXXX | POST | XXXXXXXXXXXXXXXXXXXXXXXX*/
+
+app.post("/users", (request, response) => {
+  //Using Joi to validate data
+  const schema = getSchema();
 
   const schemaResult = schema.validate(request.body);
   console.log(Boolean(schemaResult.error)); // this return a value and if conditions dont provide, it return value and error area.
@@ -79,9 +94,7 @@ app.post("/users", (request, response) => {
   if (!schemaResult.error) {
     const newUser = {
       id: users.length + 1,
-      name: request.body.name,
-      surname: request.body.surname,
-      age: request.body.age,
+      ...getRequestBody(request),
     };
     if (
       users.find((user) => user.name.toString() === newUser.name.toString())
@@ -98,6 +111,52 @@ app.post("/users", (request, response) => {
   }
 });
 
+/* XXXXXXXXXXXXXXXXXXXXXX | PUT | XXXXXXXXXXXXXXXXXXXXXXXX*/
+
+app.put("/users/:id", (request, response) => {
+  // writing with id means that which user will be updated.
+
+  const updatedUser = users.find(
+    (user) => user.id === parseInt(request.params.id)
+  );
+  if (!updatedUser) {
+    response
+      .status(404)
+      .send(`Couldn't find user with ${response.params.id} id number`);
+  } else {
+    const schema = getSchema();
+
+    const schemaResult = schema.validate(request.body);
+
+    if (!schemaResult.error) {
+      updatedUser.name = request.body.name;
+      updatedUser.surname = request.body.surname;
+      updatedUser.age = request.body.age;
+      response.send(updatedUser);
+    } else {
+      console.log("Please entry your information correctly");
+      response.status(400).send(schemaResult.error.details[0].message);
+    }
+  }
+});
+
+/* XXXXXXXXXXXXXXXXXXXXXX | DELETE | XXXXXXXXXXXXXXXXXXXXXXXX*/
+
+app.delete("/users/:id", (request, response) => {
+  const deleteUser = users.find(
+    (user) => user.id === parseInt(request.params.id)
+  );
+
+  if (deleteUser) {
+    const indexUser = users.indexOf(deleteUser);
+    users.splice(indexUser, 1); // start indexuser and delete just 1 element. So it deletes the specific element which given index
+    response.send(indexUser);
+  } else {
+    response.status(404).send("Could not find user of " + deleteUser);
+  }
+});
+
+/* app listener */
 app.listen(3000, () =>
   console.log("Server is listening with 3000 port number")
 );
